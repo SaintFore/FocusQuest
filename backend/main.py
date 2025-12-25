28e56620fe517eccc9d8283e1deee9fcae912ee6
julien <forevergoodforest@outlook.com>
2025-12-25 19:15:04 +0800

    feat: 前后端连接

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def hello_world() -> dict:
+    return {"message": "Hello, World!"}
 
 
 @app.get("/health")
 def health_check() -> dict:
-    return {"status": "ok", "hero": "ready"}
+    return {"status": "ok", "hero": "merlin"}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface HealthResponse {
+  status: string;
+  hero: string;
 }
 
-export default App
+function App() {
+  const [hero, setHero] = useState<string>("Checking");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/health")
+      .then((res) => res.json())
+      .then((data: HealthResponse) => setHero(data.hero))
+      .catch(() => setHero("Error"));
+  }, []);
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-100">
+      <div className="bg-gray-800 text-white rounded-xl px-10 py-12 shadow-lg text-center">
+        <h1 className="text-5xl font-extrabold text-purple-500 mb-8">
+          FOCUS QUEST
+        </h1>
+        <p className="text-sm text-gray-400 mb-8">Server status: {hero}</p>
+
+        <button
+          className="
+            bg-green-500
+            hover:bg-yellow-600
+            text-white
+            font-semibold
+            px-8
+            py-3
+            rounded-full
+            transition
+          "
+        >
+          Start Adventure
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
